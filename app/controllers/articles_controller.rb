69364b909fb8b3f136663a8d18ad9c0495ea3290
Zil Vyas <zvyas08@gmail.com>
2018-03-17 21:37:44 -0400

    extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update, :destroy, :show]
+
   def index
     @articles = Article.all
   end
@@ -14,7 +16,6 @@ class ArticlesController < ApplicationController
     # rails 5 output looks like:
     # <ActionController::Parameters {"title"=>"This is a title", "description"=>"THis is some description"} permitted: false>
     @article = Article.new(article_params)
-
     if @article.save
       flash[:notice] = "Article was successfully created!"
       redirect_to article_path(@article)
@@ -24,23 +25,20 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    # before_action is called here, so it can remain empty
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted!"
     redirect_to articles_path
   end
 
   def edit
-    @article = Article.find(params[:id])
+    # before_action is called here, so it can remain empty
   end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated!"
       redirect_to article_path(@article)
@@ -53,6 +51,10 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
