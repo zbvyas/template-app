377d6dfe7de20768c45c06ad5f22ae79c85d5847
Zil Vyas <zvyas08@gmail.com>
2018-03-21 19:44:06 -0400

    add minitest-reporters gem to prettify the test output

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '~> 3.1.7'
 gem 'will_paginate', '3.1.5'
 gem 'bootstrap-will_paginate', '1.0.0'
-
+gem 'minitest-reporters'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,22 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/reporters'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  # Format the test output messages
+  Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+  # The following are provided
+  # Minitest::Reporters::DefaultReporter  # => Redgreen-capable version of standard Minitest reporter
+  # Minitest::Reporters::SpecReporter     # => Turn-like output that reads like a spec
+  # Minitest::Reporters::ProgressReporter # => Fuubar-like output with a progress bar
+  # Minitest::Reporters::RubyMateReporter # => Simple reporter designed for RubyMate
+  # Minitest::Reporters::RubyMineReporter # => Reporter designed for RubyMine IDE and TeamCity CI server
+  # Minitest::Reporters::JUnitReporter    # => JUnit test reporter designed for JetBrains TeamCity
+  # Minitest::Reporters::MeanTimeReporter # => Produces a report summary showing the slowest running tests
+  # Minitest::Reporters::HtmlReporter     # => Generates an HTML report of the test results
 end
