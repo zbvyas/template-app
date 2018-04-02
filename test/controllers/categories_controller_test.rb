7cf1ee108c4e00dcaea0c8a287fb99ef649429bc
Zil Vyas <zvyas08@gmail.com>
2018-04-01 21:10:28 -0400

    fixed tests and updated navbar with categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -6,6 +6,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
     # create will save to the db (.new does not save to db)
     @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
 
   test "should get categories index" do
@@ -15,6 +16,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_response :success
   end
@@ -22,6 +24,13 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
     get category_path(@category)
     assert_response :success
+  end
+
+  test 'should redirect create when admin not logged in' do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category:{name:"sports"}}
+    end
+    assert_redirected_to categories_path
   end
 
 end
